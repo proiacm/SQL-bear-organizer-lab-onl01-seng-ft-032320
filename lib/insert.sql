976382f3d753e7bc48f51d73d09e8039ad74ad07
CiaraMaria Proia <proiacm@gmail.com>
2020-04-21 00:36:18 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'male', 'brown', 'sexy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'male', 'black', 'lazy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'playful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'female', 'brown', 'stoic', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'female', 'black', 'loud', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 6, 'female', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'female', 'black', 'shy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, 'male', 'brown', NULL, 0);
